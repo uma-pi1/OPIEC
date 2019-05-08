77902fe83c4d4fdd41f937a94b25cb6a2f15f586
Kiril <kgashteovski@informatik.uni-mannheim.de>
2019-05-08 19:38:14 +0200

    read data with java

--- src/main/java/de/uni_mannheim/ReadArticlesAvro.java
+++ src/main/java/de/uni_mannheim/ReadArticlesAvro.java
@@ -1,4 +1,23 @@
 package de.uni_mannheim;
 
+import avroschema.linked.WikiArticleLinkedNLP;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.avro.file.DataFileReader;
+import org.apache.avro.io.DatumReader;
+import org.apache.avro.specific.SpecificDatumReader;
+
 public class ReadArticlesAvro {
+    public static void main(String args[]) throws IOException {
+        File f = new File("data/articles.avro");
+        DatumReader<WikiArticleLinkedNLP> userDatumReader = new SpecificDatumReader<>(WikiArticleLinkedNLP.class);
+        DataFileReader<WikiArticleLinkedNLP> dataFileReader = new DataFileReader<>(f, userDatumReader);
+
+        while (dataFileReader.hasNext()) {
+            WikiArticleLinkedNLP article = dataFileReader.next();
+            System.out.println("Processing article: " + article.getTitle());
+        }
+    }
 }
--- src/main/java/de/uni_mannheim/ReadTriplesAvro.java
+++ src/main/java/de/uni_mannheim/ReadTriplesAvro.java
@@ -1,7 +1,23 @@
 package de.uni_mannheim;
 
+import avroschema.linked.TripleLinked;
+import org.apache.avro.file.DataFileReader;
+import org.apache.avro.io.DatumReader;
+import org.apache.avro.specific.SpecificDatumReader;
+
+import java.io.File;
+import java.io.IOException;
+
 public class ReadTriplesAvro {
-    public static void main(String args[]) {
-        System.out.println("Hello World!");
+    public static void main(String args[]) throws IOException {
+        File f = new File("data/triples.avro");
+        DatumReader<TripleLinked> userDatumReader = new SpecificDatumReader<>(TripleLinked.class);
+        DataFileReader<TripleLinked> dataFileReader = new DataFileReader<>(f, userDatumReader);
+
+        while (dataFileReader.hasNext()) {
+            // Debugging variables
+            TripleLinked triple = dataFileReader.next();
+            System.out.print("Processing triple: " + triple.getTripleId());
+        }
     }
 }
